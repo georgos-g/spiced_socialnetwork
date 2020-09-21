3d4742f9df2d56bed16093dff48ce7e3376e3956
georgos-g <gg@dancingbear.de>
2020-09-21 23:22:33 +0200

    merge

--- src/Chat.js
+++ src/Chat.js
@@ -18,34 +18,41 @@ export default function Chat() {
     };
     console.log("chatMessages: ", chatMessages);
 
-    return ( <div><div> <h2>The Secret Chat</h2></div>
-            <div className="chat">
-           
-            <div className="chat-content">
-                {chatMessages.map((message) => (
-                    <div key={message.chats_id} className="chat_single">
-                        <Link to={"/user/" + message.user_id}>
-                            <div>
-                                <img src={message.profile_picture_url} />
-
-                                <div id="chat_name">
-                                    {message.firstname} {message.lastname}
-                                </div>
-                            </div>
-                        </Link>
-
-                        {/* <div className='chat-message-date'>{message.created_at}</div> */}
-                        <div className="chat-text">{message.message_text}</div>
-                    </div>
-                ))}
-
-                <textarea
-                    className="chat_input_field"
-                    placeholder="Tell us your SECRET and press Enter"
-                    onKeyDown={sendMsgButton}
-                />
+    return (
+        <div>
+            <div>
+                {" "}
+                <h2>The Secret Chat</h2>
             </div>
-        </div></div>
+            <div className="chat">
+                <div className="chat-content">
+                    {chatMessages.map((message) => (
+                        <div key={message.chats_id} className="chat_single">
+                            <Link to={"/user/" + message.user_id}>
+                                <div>
+                                    <img src={message.profile_picture_url} />
+
+                                    <div id="chat_name">
+                                        {message.firstname} {message.lastname}
+                                    </div>
+                                </div>
+                            </Link>
+
+                            {/* <div className='chat-message-date'>{message.created_at}</div> */}
+                            <div className="chat-text">
+                                {message.message_text}
+                            </div>
+                        </div>
+                    ))}
+
+                    <textarea
+                        className="chat_input_field"
+                        placeholder="Tell us your SECRET and press Enter"
+                        onKeyDown={sendMsgButton}
+                    />
+                </div>
+            </div>
+        </div>
     );
-    //----
+    
 }
